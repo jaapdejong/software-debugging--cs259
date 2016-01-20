efd1a0e2f1c12b350839fda90d264db881e34f0e
Jaap de Jong <jaap.dejong@nedap.com>
2016-01-20 21:12:31 +0100

    Add lesson 3 exercise 1

---
+++ lesson 3/exercise1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# Finish the simplify function.
+# Have it deal with the case where test(s1) doesn't fail
+# 
+# If neither s1 or s2 cause the test to fail then return 
+# the original string, s.
+
+import re
+
+def test(s):
+	print s, len(s)
+	if re.search("<SELECT[^>]*>", s) >= 0:
+#		print s, len(s), "FAIL"
+		return "FAIL"
+	else:
+#		print s, len(s), "PASS"
+		return "PASS"
+
+def simplify(s):
+	assert test(s) == "FAIL"
+
+	split = len(s) / 2
+	s1 = s[:split]
+	s2 = s[split:]
+
+	if test(s1) == "FAIL":
+		return simplify(s1)
+	# YOUR CODE HERE
+	if test(s2) == "FAIL":
+		return simplify(s2)
+	return s
+
+# UNCOMMENT TO TEST
+html_input = '<SELECT>foo</SELECT>'
+#html_input = '<SELECT>'
+print simplify(html_input)
+
